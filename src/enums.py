c4821f3e789f8849f66f53ad5f31937380b9d9c5
Ryley McRae <ryleymcrae@gmail.com>
2024-10-06 10:16:39 -0700

    Added MappedStringEnum

--- src/enums.py
+++ src/enums.py
@@ -1,29 +1,24 @@
 from enum import Enum
 
 
-class RackingPattern(Enum):
-    CONTINUOUS = "Continuous"
-    STAGGERED = "Staggered"
-
+class MappedStringEnum(Enum):
     def __str__(self) -> str:
         return self.value
 
     @classmethod
     def map(cls):
-        return {str(e): e for e in cls}
+        return {e.value: e for e in cls}
 
 
-class RafterSpacing(Enum):
+class RackingPattern(MappedStringEnum):
+    CONTINUOUS = "Continuous"
+    STAGGERED = "Staggered"
+
+
+class RafterSpacing(MappedStringEnum):
     TWELVE = "12"
     SIXTEEN = "16"
     NINETEEN_AND_THREE_SIXTEENTHS = "19.1875"
     TWENTY_FOUR = "24"
     THIRTY_TWO = "32"
     FORTY_EIGHT = "48"
-
-    def __str__(self) -> str:
-        return self.value
-
-    @classmethod
-    def map(cls):
-        return {str(e): e for e in cls}
