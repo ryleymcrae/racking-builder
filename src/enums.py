1aa350abf8d86dd39d4d77a0c684ebe906751245
Ryley McRae <ryleymcrae@gmail.com>
2024-10-06 09:52:27 -0700

    Removed PanelType Enum

--- src/enums.py
+++ src/enums.py
@@ -1,28 +1,6 @@
 from enum import Enum
 
 
-class PanelType(Enum):
-    LONGI505 = ("LONGi 505W", 2094, 1134)
-
-    def __init__(self, *values) -> None:
-        self.values = values
-
-    def __str__(self) -> str:
-        return self.values[0]
-
-    @property
-    def height_inches(self) -> float:
-        return round(self.values[1] / 25.4, 4)
-
-    @property
-    def width_inches(self) -> float:
-        return round(self.values[2] / 25.4, 4)
-
-    @classmethod
-    def map(cls):
-        return {str(e): e for e in cls}
-
-
 class RackingPattern(Enum):
     CONTINUOUS = "Continuous"
     STAGGERED = "Staggered"
