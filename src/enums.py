7b99dc1bf8c3822642bd9f76d674b34a8ecc2e71
Ryley McRae <39610429+ryleymcrae@users.noreply.github.com>
2024-10-28 11:38:46 -0700

    Remove RafterSpacing enum

--- src/enums.py
+++ src/enums.py
@@ -13,12 +13,3 @@ class MappedStringEnum(Enum):
 class RackingPattern(MappedStringEnum):
     CONTINUOUS = "Continuous"
     STAGGERED = "Staggered"
-
-
-class RafterSpacing(MappedStringEnum):
-    TWELVE = "12"
-    SIXTEEN = "16"
-    NINETEEN_AND_THREE_SIXTEENTHS = "19.1875"
-    TWENTY_FOUR = "24"
-    THIRTY_TWO = "32"
-    FORTY_EIGHT = "48"
